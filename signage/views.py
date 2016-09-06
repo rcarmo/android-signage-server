522e5d3983a809e840772acdf83d43f224322168
Rui Carmo <rui.carmo@gmail.com>
2016-09-06 23:24:23 +0100

    TODO

--- signage/views.py
+++ signage/views.py
@@ -7,6 +7,9 @@ from .models import Playlist, Device
 
 # Create your views here.
 
+# TODO: handle device id
+# TODO: handle alerts
+
 class PlaylistView(generic.TemplateView):
     template_name = 'playlist.json'
 
