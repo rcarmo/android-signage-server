666bbb905226b0c15959313a1d2a03a71ca9c550
Rui Carmo <rui.carmo@gmail.com>
2016-09-10 19:42:32 +0100

    Enable WAL

---
+++ signage/__init__.py
@@ -0,0 +1,9 @@
+from django.db.backends.signals import connection_created
+
+def activate_wal(sender, connection, **kwargs):
+    """Enable WAL mode."""
+    if connection.vendor == 'sqlite':
+        cursor = connection.cursor()
+        cursor.execute('PRAGMA journal_mode=WAL;')
+
+connection_created.connect(activate_foreign_keys)
