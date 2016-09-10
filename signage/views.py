460c97261f133f4177c9f81c1dd3e86f8f599ed1
Rui Carmo <rui.carmo@gmail.com>
2016-09-10 22:24:46 +0100

    Refactoring

--- signage/views.py
+++ signage/views.py
@@ -1,66 +1,84 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.views import generic, View
+from django.views.generic import TemplateView, DetailView
+from django.utils import timezone
 from uuid import uuid5, NAMESPACE_URL
-from .models import Playlist, Device
+from .models import Playlist, Device, Alert
+from datetime import datetime
 
 # Create your views here.
 
 # TODO: handle alerts
 
-class PlaylistView(generic.TemplateView):
+class PlaylistView(TemplateView):
     template_name = 'playlist.json'
-    default_playlist = None
+    playlist = None
 
-    def get_or_update_device(self):
+    def get_or_update_device(self, kwargs):
         try:
-            device = Device.objects.get(device_id=self.kwargs['device_id'])
+            device = Device.objects.get(device_id=kwargs['device_id'])
             dirty = False
-            if device.ip_address != self.kwargs['ip_address']:
-                device.ip_address = self.kwargs['ip_address']
+            if device.ip_address != kwargs['ip_address']:
+                device.ip_address = kwargs['ip_address']
                 dirty = True
-            if device.mac_address != self.kwargs['mac_address']:
-                device.mac_address = self.kwargs['mac_address']
+            if device.mac_address != kwargs['mac_address']:
+                device.mac_address = kwargs['mac_address']
                 dirty = True
             if dirty:
                 device.save()
         except:
-            device = Device(device_id = self.kwargs['device_id'],
-                     mac_address = self.kwargs['mac_address'],
-                     ip_address = self.kwargs['ip_address'])
+            device = Device(device_id = kwargs['device_id'],
+                     mac_address = kwargs['mac_address'],
+                     ip_address = kwargs['ip_address'])
             device.save()
             
         return device
 
-
-    def get_playlist(self):
-        device = self.get_or_update_device()
+    def get_context_data(self, **kwargs):
+        context = super(PlaylistView, self).get_context_data(**kwargs)
+        device = self.get_or_update_device(kwargs)
+        alert = False
+        print device
         if device.active:
-            if device.playlist:
-                return device.playlist
+            # do we have pending alerts for this device?
+            print "active"
+            alerts = list(Alert.objects.filter(
+                active=True,
+                when__lte=timezone.now(),
+                #devices__pk__exact=device.pk
+            ).exclude(
+                #shown_on__pk__exact=device.pk
+            ).order_by('when').distinct()[:1])
+            print alerts
+            if alerts:
+                alert = alerts[0]
+                self.alert = True
+                alert.shown_on.add(device)
+                if set(list(alert.shown_on.all())) == set(list(alert.devices.all())):
+                    alert.active = False 
+                alert.save()
+                playlist = alert
+            elif device.playlist:
+                playlist = device.playlist
             try:
-                return Playlist.objects.get(name='Default')
+                playlist = Playlist.objects.get(name='Default')
             except:
-                return Playlist.objects.get(pk=1)
-        return Playlist()
+                playlist = Playlist.objects.get(pk=1)
+        else:
+            playlist = Playlist()
+        context['playlist'] = playlist
+        context['uuid'] = self.uuid(playlist)
+        context['assets'] = playlist.asset_set.order_by('asset_order')
 
+        return context
 
-    def uuid(self):
+    def uuid(self, playlist):
         acc = []
-        for a in self.get_playlist().asset_set.order_by('asset_order'):
+        for a in playlist.asset_set.order_by('asset_order'):
             acc.append((a.url, a.duration, a.active, a.kind))
         return uuid5(NAMESPACE_URL, str(acc))
 
-
-    def name(self):
-        return self.get_playlist().name
-
-
-    def assets(self):
-        return self.get_playlist().asset_set.order_by('asset_order')
-
-
-class DetailView(generic.DetailView):
+class DetailView(DetailView):
     model = Playlist
     template_name = 'detail.json'
