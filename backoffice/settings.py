adf60a5a0cd160b6ac9379420d8c2b6979bc7249
Rui Carmo <rui.carmo@gmail.com>
2016-10-02 16:33:53 +0100

    Add timeout to cover for sqlite3

--- backoffice/settings.py
+++ backoffice/settings.py
@@ -78,6 +78,9 @@ WSGI_APPLICATION = 'backoffice.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'OPTIONS' = {
+            'timeout': 20,
+        },
         'NAME': os.environ.get('DATABASE_PATH',os.path.join(BASE_DIR, 'db.sqlite3')),
     }
 }
